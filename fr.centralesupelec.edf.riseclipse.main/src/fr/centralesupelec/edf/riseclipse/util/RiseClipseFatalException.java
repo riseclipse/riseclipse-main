9c92b9ff6e4424244b27d27f88374ef45798a6ce
Dominique Marcadet <Dominique.Marcadet@centralesupelec.fr>
2018-04-10 18:12:40 +0200

    Copyright comment updated

--- fr.centralesupelec.edf.riseclipse.main/src/fr/centralesupelec/edf/riseclipse/util/RiseClipseFatalException.java
+++ fr.centralesupelec.edf.riseclipse.main/src/fr/centralesupelec/edf/riseclipse/util/RiseClipseFatalException.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright (c) 2017 CentraleSupélec & EDF.
+ *  Copyright (c) 2018 CentraleSupélec & EDF.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -8,9 +8,13 @@
  *  This file is part of the RiseClipse tool
  *  
  *  Contributors:
- *      Computer Science Department, CentraleSupélec : initial implementation
+ *      Computer Science Department, CentraleSupélec
+ *      EDF R&D
  *  Contacts:
- *      Dominique.Marcadet@centralesupelec.fr
+ *      dominique.marcadet@centralesupelec.fr
+ *      aurelie.dehouck-neveu@edf.fr
+ *  Web site:
+ *      http://wdi.supelec.fr/software/RiseClipse/
  */
 package fr.centralesupelec.edf.riseclipse.util;
 
